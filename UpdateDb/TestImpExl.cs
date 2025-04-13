589c2bd9712e1c9bc06b5fe4ac981a4dfbc8db80
Roberto Carlucci <roby.crl@gmail.com>
2025-04-13 19:08:56 +0200

    20250413 PM Casa

--- UpdateDb/TestImpExl.cs
+++ UpdateDb/TestImpExl.cs
@@ -15,69 +15,86 @@ namespace PvPmo.UpdateDb
             foreach (var n in _finalizza.caricaTabFin) 
             {
                 string? azione = n.Azione.ToString();
-                string? tabConf = n.TabConfronto.ToString();
-                string? colConf = n.ColConfronto.ToString();
-                string? tabTest = n.TabTestare.ToString();
-                string? colTest = n.ColDaTestare.ToString();
-                string? dbConf = n.DbTabConfronto.ToString();
-                string? dbTest = n.DbTabTest.ToString();
-                Bol = await TestFileExlImp(azione, tabConf, colConf, tabTest, colTest,dbConf, dbTest);
+                string? tabProd = n.TabConfronto.ToString();
+                string? colProd = n.ColConfronto.ToString();
+                string? tabUptd = n.TabTestare.ToString();
+                string? colUptd = n.ColDaTestare.ToString();
+                string? dbProd = n.DbTabConfronto.ToString();
+                string? dbUptd = n.DbTabTest.ToString();
+                Bol = await TestFileExlImp(azione, tabProd, colProd, tabUptd, colUptd, dbProd, dbUptd);
             }
             return Bol;
         }
         // Eseguo i test sui file exl di update.
         public static async Task<bool> TestFileExlImp(
-            string _azione, string _tabConf, string _colConf, string _tabTest, string _colTest, string _dbConf, string _dbTest)
+            string _azione, string _tabProd, string _colProd, string _tabUptd, string _colUptd, string _dbProd, string _dbUptd)
         {
             // Test corrisponde alla tabella Uptd.
-            DataTable dataTest = new DataTable();
-            DataTable dataConf = new DataTable();
+            DataTable dataProd = new DataTable();
+            DataTable dataUptd = new DataTable();
+            
             bool Bol = false;
-            string StrConnConf = Conn.MysqlConn(_dbConf);
-            string StrConnTest = Conn.MysqlConn(_dbTest);
+            string StrConnProd = Conn.MysqlConn(_dbProd);
+            string StrConnUptd = Conn.MysqlConn(_dbUptd);
 
             switch (_azione)
             {
                 //Test Valore Data e Record Data presenti in Uptd
                 case "TST":
-                    // Viene effettuato il test delle date presenti in Uptd.
-                    string QrySqlTest = "SELECT DISTINCT " + _colTest + " FROM " + _tabTest + " ORDER BY " + _colTest + " ASC;";
-                    string QrySqlConf = "SELECT " + _colConf + " FROM " + _tabConf + " ORDER BY " + _colConf + " ASC;";
+                    // Viene effettuato il test del numero di righe della data presenti in Uptd
+                    // Confrontandolo con la tabella 01_tabella_data del Db di produzione.
+                    string QrySqlProd = "SELECT " + _colProd + " FROM " + _tabProd + " ORDER BY " + _colProd + " ASC;";
+                    string QrySqlUptd = "SELECT DISTINCT " + _colUptd + " FROM " + _tabUptd + " ORDER BY " + _colUptd + " ASC;";
 
-                    dataTest = await SqlAsync.SqlQryDataTable(StrConnTest, QrySqlTest, dataTest, 60);
-                    dataConf = await SqlAsync.SqlQryDataTable(StrConnConf, QrySqlConf, dataConf, 60);
-                    DataRow[] temp = dataConf.Select();
+                    dataProd = await SqlAsync.SqlQryDataTable(StrConnProd, QrySqlProd, dataProd, 60);
+                    dataUptd = await SqlAsync.SqlQryDataTable(StrConnUptd, QrySqlUptd, dataUptd, 60);
+                    DataRow[] temp = dataProd.Select();                    
+                    
                     foreach (DataRow row in temp)
                     {
-                        string? res = row[_colConf].ToString();
+                        string? res = row[_colProd].ToString();
                         if (string.IsNullOrEmpty(res))
                         {
-                            dataConf.Rows.Remove(row);
+                            dataProd.Rows.Remove(row);
                         }
                     }
-                    var countTest = dataTest.Rows.Count;
-                    var countConf = dataConf.Rows.Count;
-                    if (countTest != countConf) { Bol = true; }
+                    var countProd = dataProd.Rows.Count;
+                    var countUptd = dataUptd.Rows.Count;                    
+                    if (countUptd != countProd) 
+                    {
+                        await Shell.Current.DisplayAlert
+                            ("Test numero righe  data !", $"Le  non corrispondono.", "Ok");
+                        Bol = true; 
+                    }
                     
-                    // Viene testato il valore delle date
-                    var result = dataConf.AsEnumerable().Intersect(dataTest.AsEnumerable(), DataRowComparer.Default);
-                    if (result == null) { Bol = true; }
-
+                    // Viene testato il valore delle date se corrispondenti.
+                    var result = dataProd.AsEnumerable().Intersect(dataUptd.AsEnumerable(), DataRowComparer.Default);
+                    if (result != null) 
+                    {
+                        await Shell.Current.DisplayAlert
+                            ("Test Valore date !", $"Le date non corrispondono.", "Ok");
+                        Bol = true; 
+                    }                    
                     break;
                 case "NUM":
-                    // Viene testato il numero delle colonne presenti in Uptd.
-                    QrySqlTest = "SELECT * FROM `information_schema`.`COLUMNS` WHERE TABLE_SCHEMA = '"
-                        + _dbTest + "' AND TABLE_NAME = '" + _tabTest + "' ORDER BY ORDINAL_POSITION;";
-                    QrySqlConf = "SELECT * FROM `information_schema`.`COLUMNS` WHERE TABLE_SCHEMA = '"
-                        + _dbConf + "' AND TABLE_NAME = '" + _tabConf + "' ORDER BY ORDINAL_POSITION;";
-                    
-                    await SqlAsync.SqlQryDataReader(StrConnTest, QrySqlTest, dataTest, 30);
-                    await SqlAsync.SqlQryDataReader(StrConnConf, QrySqlConf, dataConf, 30);
+                    // Viene testato il numero delle colonne presenti in Uptd rispetto alle tabelle presenti
+                    // nel Db di produzione.                    
+                    QrySqlProd = "SHOW COLUMNS FROM '" + _tabProd + ";";
+                    QrySqlUptd = "SHOW COLUMNS FROM '" + _tabUptd + ";";
 
-                    int _dif = dataTest.Rows.Count - dataConf.Rows.Count;
-                    if (_dif != 0) { return true; }
+                    await SqlAsync.SqlQryDataReader(StrConnUptd, QrySqlUptd, dataUptd, 30);
+                    await SqlAsync.SqlQryDataReader(StrConnProd, QrySqlProd, dataProd, 30);
 
+                    int _dif = dataUptd.Rows.Count - dataProd.Rows.Count;
+                    if (_dif != 0) 
+                    {
+                        await Shell.Current.DisplayAlert
+                            ("Test Numero colonne !", $"Il numero delle colonne non corrisponde.", "Ok");
+                        return true; 
+                    }                    
                     break;
+                    // Vengono cambiati i nomi colonna adeguandoli a quelli delle tabelle presenti
+                    // nel Db di produzione.
                 case "REN":
 
                     break;
